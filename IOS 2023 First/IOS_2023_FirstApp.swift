5c755260a093f8b9c00477837694fc8464138b79
Ikshit C <109130289+Ikshit-67@users.noreply.github.com>
2023-01-04 20:07:50 +0530

    Initial Commit

---
+++ IOS 2023 First/IOS_2023_FirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_2023_FirstApp.swift
+//  IOS 2023 First
+//
+//  Created by Ikshit Chaudhari on 04/01/23.
+//
+
+import SwiftUI
+
+@main
+struct IOS_2023_FirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
