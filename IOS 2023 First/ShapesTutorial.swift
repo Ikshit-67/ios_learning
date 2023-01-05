105f0bce0a10a1eceb8bfdbd33fcd702955b0c71
Ikshit C <109130289+Ikshit-67@users.noreply.github.com>
2023-01-05 19:34:14 +0530

    shapes done

--- IOS 2023 First/ShapesTutorial.swift
+++ IOS 2023 First/ShapesTutorial.swift
@@ -11,7 +11,20 @@ struct ShapesTutorial: View {
     var body: some View {
         
         // To draw a circle : Circle()
-        Circle()
+        
+//        Circle()
+//            .foregroundColor(.pink)
+//            .stroke(.pink, lineWidth: 12)
+//            .frame(width: 150, height: 150)
+        
+//        Rectangle()
+//            .frame(width: 300, height: 150)
+//            .cornerRadius(60)
+//            .foregroundColor(.blue)
+        
+        Capsule()
+            .stroke(.cyan, lineWidth: 5)
+            .frame(width: 100, height: 40)
     }
 }
 
