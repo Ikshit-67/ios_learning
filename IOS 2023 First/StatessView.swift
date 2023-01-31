310caf920c34a63f4eb03eeb98f25a1ec3106bae
Ikshit C <109130289+Ikshit-67@users.noreply.github.com>
2023-01-31 10:15:48 +0530

    states dome

--- IOS 2023 First/StatessView.swift
+++ IOS 2023 First/StatessView.swift
@@ -11,6 +11,9 @@ struct StatessView: View {
     
     @State var countt = 0
     
+    @State var numLikes = 0
+    @State var likeImageShape = "heart"
+    @State var likeImageColor = Color.black
     var body: some View {
         VStack{
             
@@ -44,6 +47,101 @@ struct StatessView: View {
                         .clipShape(Circle())
                 }
 
+            }
+            
+            Divider()
+//            Spacer()
+            VStack(alignment: .leading){
+                
+                // User profile photu and username
+                HStack{
+                    Image("pop")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 35, height: 35)
+                        .clipShape(Circle())
+                    
+                    Text("popmusicfan")
+                        .fontWeight(.semibold)
+                        .font(.callout)
+                    
+                    Spacer()
+                    
+                    Image(systemName: "ellipsis")
+                }
+                .padding(.horizontal)
+                
+                // image
+                Image("podcast2")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 400)
+                    .clipped()
+                
+                // like coment share
+                HStack(spacing: 25){
+                    // like button start
+                    Button {
+                        likeImageShape = "heart.fill"
+                        likeImageColor = Color.red
+                        numLikes += 1
+                    } label: {
+                        Image(systemName: likeImageShape)
+                            .font(.system(size: 22.5, weight: .semibold))
+                            .foregroundColor(likeImageColor)
+                        
+                    }
+                    // like button end
+                    
+                    
+                    // comment button start
+                    Button {
+                        // comment func
+                    } label: {
+                        Image(systemName: "text.bubble")
+                            .font(.system(size: 22.5, weight: .semibold))
+                            .foregroundColor(.black)
+                        
+                    }
+                    // comment button end
+                    
+                    
+                    
+                    // share button start
+                    Button {
+                        // share func
+                    } label: {
+                        Image(systemName: "paperplane")
+                            .font(.system(size: 22.5, weight: .semibold))
+                            .foregroundColor(.black)
+                        
+                    }
+                    // share button end
+                    
+                    Spacer()
+                }
+                .padding(.leading)
+                .padding(.top, 2.5)
+                
+                // number of likes
+                Text("\(numLikes) likes")
+                    .font(.system(size: 15, weight: .semibold))
+                    .padding(.leading)
+                    .padding(.top, 2.5)
+                
+                
+                HStack{
+                    Text("popmusicfan")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                    
+                    Text("podcaster, rockboi, ambionce")
+                        .font(.footnote)
+                }
+                .padding(.leading)
+                .padding(.top, 2.5)
+                    
+                
             }
             
         }
