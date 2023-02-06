59312688f455b923053ea079a40599bb64a3b53b
Ikshit C <109130289+Ikshit-67@users.noreply.github.com>
2023-02-06 15:03:50 +0530

    control flow mastered

--- IOS 2023 First/ControlFlowView.swift
+++ IOS 2023 First/ControlFlowView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ControlFlowView: View {
     
-    @State var darkModeOn = false
+    @State var darkModeOn: Bool = false
+    
+    @State var notificationsOn: Bool = false
     
     var rowBgColor: Color {
         return darkModeOn ? Color.white.opacity(0.3) : Color(.systemGroupedBackground)
@@ -27,17 +29,23 @@ struct ControlFlowView: View {
         return darkModeOn ? Color.blue : Color.black
     }
     
+    var notificationsColor: Color {
+        return notificationsOn ? Color.blue : Color.red
+    }
 
     
     var body: some View {
         
-        VStack(alignment: .leading, spacing: 20){
+        VStack(alignment: .leading, spacing: 40){
             
             Text("Settings")
                 .font(.largeTitle)
                 .fontWeight(.semibold)
                 .foregroundColor(fgColor)
         
+//        Theme settings Vstack
+//         \ | | /
+//          ------
             VStack(alignment: .leading, spacing: 10){
                 Text("Theme")
                     .font(.title3)
@@ -47,7 +55,7 @@ struct ControlFlowView: View {
                 Toggle(isOn: $darkModeOn) {
                     HStack{
                         Image(systemName: "moon.circle.fill")
-                            .font(.title2)
+                            .font(.title)
                             .imageScale(.medium)
                             .foregroundColor(iconFgColor)
                         
@@ -60,6 +68,42 @@ struct ControlFlowView: View {
                 .tint(.blue)
                 .padding(.horizontal)
                 .frame(height: 45)
+                .background(rowBgColor)
+                .cornerRadius(7.5)
+            }
+            
+//       Notifications settings Vstack
+//         \ | | /
+//          ------
+            VStack(alignment: .leading){
+                Text("Notifications")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .foregroundColor(fgColor)
+                
+                Toggle(isOn: $notificationsOn) {
+                    HStack(){
+                        Image(systemName: "bell.circle.fill")
+                            .font(.title)
+                            .imageScale(.medium)
+                            .foregroundColor(notificationsColor)
+                        
+                        VStack(alignment: .leading){
+                            
+                            Text("Enable notifications")
+                                .font(.callout)
+                                .fontWeight(.medium)
+                            
+                            Text("Notifications are \((notificationsOn) ? "on" : "off")")
+                                .font(.footnote)
+                            
+                        }
+                        .foregroundColor(fgColor)
+                    }
+                }
+                .tint(.blue)
+                .padding(.horizontal)
+                .frame(height: 75)
                 .background(rowBgColor)
                 .cornerRadius(7.5)
             }
